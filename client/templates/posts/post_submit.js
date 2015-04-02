6e26860c8a89a9a8e9ed50f217140c72a09c9a4b
Joris <joris@orikami.nl>
2015-04-02 17:38:23 +0200

    validate post contents on submission

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,17 @@
+Template.postSubmit.created = function() {
+    Session.set('postSubmitErrors', {});
+}
+Template.postSubmit.helpers({
+    errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+},
+errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+    }
+});
+
+
+
 Template.postSubmit.events({
     'submit form': function(e) {
         e.preventDefault();
@@ -6,14 +20,19 @@ Template.postSubmit.events({
             title: $(e.target).find('[name=title]').val()
         };
 
-        Meteor.call('postInsert', post, function(error, result){
-        	//display the error to the user and abort
-        	if (error)
-        		return throwError(error.reason);
+        var errors = validatePost(post);
+        if (errors.title || errors.url)
+            return Session.set('postSubmitErrors', errors);
 
-        	if (result.postExists)
-        		throwError('This link has already been posted');
-        	
+        Meteor.call('postInsert', post, function(error, result){
+    	//display the error to the user and abort
+    	if (error)
+    		return throwError(error.reason);
+        
+        // show this result but route anyway
+    	if (result.postExists)
+    		throwError('This link has already been posted');
+    	
         Router.go('postPage', {_id: result._id});
     });
     }
