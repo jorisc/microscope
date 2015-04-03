9fcb67eddcb5f74a5f3f32fcf22b49104b87955c
Joris <joris@orikami.nl>
2015-04-03 14:28:35 +0200

    use single post sub

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,18 @@
+// Meteor.publish('posts', function(options) {
+//   check(options, {
+//     sort: Object,
+//     limit: Number
+//   });
+// 	return Posts.find({}, options);
+// });
+
 Meteor.publish('posts', function(options) {
-  check(options, {
-    sort: Object,
-    limit: Number
-  });
-	return Posts.find({}, options);
+  return Posts.find({}, options);
+});
+
+Meteor.publish('singlePost', function(id) {
+  check(id, String)
+  return Posts.find(id);
 });
 
 Meteor.publish('comments', function(postId) {
